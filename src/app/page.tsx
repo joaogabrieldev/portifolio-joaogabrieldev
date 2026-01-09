fd4f9b5ee0fb98f243b265f49f62408a62e71534
joaogabrieldev0 <joaogabriel2r.profissional@hotmail.com>
2026-01-09 00:23:57 -0300

    Desktop Version of this project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import DesktopPageContent from "@/widgets/Desktop/DesktopPageContent/DesktopPageContent";
 import MobilePageContent from "@/widgets/Mobile/NonDesktopPageContent/NonDesktopPageContent";
 
 export default function Home() {
   return (
     <div>
-      <MobilePageContent />
+      {/* <MobilePageContent /> */}
+      <DesktopPageContent />
     </div>
   );
 }
